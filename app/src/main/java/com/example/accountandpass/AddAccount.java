3586c3e29cdf3e5fd3ac261754b29611b2528367
Linh Trần <trung28899@gmail.com>
2020-02-29 19:34:41 -0500

    working on UI

---
+++ app/src/main/java/com/example/accountandpass/AddAccount.java
@@ -0,0 +1,14 @@
+package com.example.accountandpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_account);
+    }
+}
